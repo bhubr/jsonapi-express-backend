7b13e379deaef82ae6d6056d36935bb72d875451
Benoît Hubert <benoithubert@gmail.com>
2017-02-23 10:01:29 +0100

    restore manual timestamps assignment

--- jsonapi.js
+++ jsonapi.js
@@ -120,15 +120,34 @@ function mapRecordToPayload( record, type, attributes ) {
   return { id: record.id, type, attributes };
 }
 
+/**
+ * You first need to create a formatting function to pad numbers to two digits…
+ **/
+function twoDigits(d) {
+    if(0 <= d && d < 10) return "0" + d.toString();
+    if(-10 < d && d < 0) return "-0" + (-1*d).toString();
+    return d.toString();
+}
+
+/**
+ * …and then create the method to output the date string as desired.
+ * Some people hate using prototypes this way, but if you are going
+ * to apply this to more than one Date object, having it as a prototype
+ * makes sense.
+ **/
+Date.prototype.toMysqlFormat = function() {
+    return this.getUTCFullYear() + "-" + twoDigits(1 + this.getUTCMonth()) + "-" + twoDigits(this.getUTCDate()) + " " + twoDigits(this.getUTCHours()) + ":" + twoDigits(this.getUTCMinutes()) + ":" + twoDigits(this.getUTCSeconds());
+};
+
 function processAttributes( attributes, doCreate ) {
-  // let updatedAt = new Date().toMysqlFormat();
+  let updatedAt = new Date().toMysqlFormat();
   let outputAttrs = Object.assign( {},
-    utils.lowerCamelAttributes( attributes )
-    // { updatedAt }
+    utils.lowerCamelAttributes( attributes ),
+    { updatedAt }
   );
-  // if( doCreate ) {
-  //   outputAttrs.createdAt = updatedAt;
-  // }
+  if( doCreate ) {
+    outputAttrs.createdAt = updatedAt;
+  }
   return outputAttrs;
 }
 
