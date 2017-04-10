d61430925b69ad7b1f296d955040d0e20a2e31bb
Benoît Hubert <benoithubert@gmail.com>
2017-04-10 17:44:54 +0200

    Fixes

--- index.js
+++ index.js
@@ -1,30 +1,8 @@
 
 
-module.exports = function(keysFolder) {
-  const { generateJwt, checkJwt, checkJwtMiddleware } = require('./lib/authToken')(keysFolder);
-  const { router, middlewares } = require('./lib/jsonapi');
-  middlewares.checkJwt = checkJwtMiddleware;
-
-  router.post('/signin', (req, res) => {
-    const { email, password } = req.body.data.attributes;
-    const selectUserQuery = queryBuilder.selectWhere('users', { email });
-    queryAsync(selectUserQuery)
-    .then(users => {
-      if (! users.length) {
-        return res.status(401).send('no account with this email');
-      }
-      const user = users[0];
-      bcrypt.compareAsync(user.password, password)
-      // .then(() => user.getPermissions())
-      .then(() => generateJwt(user))
-      .then(jwt => (res.jsonApi({ userId: user.id, jwt })));
-    })
-    .catch(err => {
-      console.log(err);
-        // return done(null, false, { message: 'Incorrect password => ' + err });
-        return res.status(401).send(err);
-      });
-  });
+module.exports = function(baseDir, config) {
+  const { generateJwt, checkJwt, checkJwtMiddleware } = require('./lib/authToken')(baseDir, config);
+  const { router, middlewares } = require('./lib/jsonapi')(config, generateJwt, checkJwtMiddleware);
 
   return { router, middlewares };
 }
