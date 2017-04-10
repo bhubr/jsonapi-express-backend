9fd9464e70ff5da3576d186dc1fc862a90f4b71a
Benoît Hubert <benoithubert@gmail.com>
2017-04-11 01:02:19 +0200

    Handle relationships (accept external relationships map)

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 
-module.exports = function(baseDir, config) {
+module.exports = function(baseDir, config, modelRelationships) {
   const { generateJwt, checkJwt, checkJwtMiddleware } = require('./lib/authToken')(baseDir, config);
-  const { router, middlewares } = require('./lib/jsonapi')(config, generateJwt, checkJwtMiddleware);
+  const { router, middlewares } = require('./lib/jsonapi')(config, generateJwt, checkJwtMiddleware, modelRelationships);
 
   return { router, middlewares };
 }
