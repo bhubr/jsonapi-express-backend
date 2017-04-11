5b8498e2fcd56c83e7402d77dd2b4b5cacbb826d
Benoît Hubert <benoithubert@gmail.com>
2017-04-11 16:13:34 +0200

    Expose queryAsync

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 module.exports = function(baseDir, config, modelRelationships) {
   const { generateJwt, checkJwt, checkJwtMiddleware } = require('./lib/authToken')(baseDir, config);
-  const { router, middlewares } = require('./lib/jsonapi')(config, generateJwt, checkJwtMiddleware, modelRelationships);
+  const { router, middlewares, queryAsync } = require('./lib/jsonapi')(config, generateJwt, checkJwtMiddleware, modelRelationships);
   const queryBuilder = require('./lib/queryBuilder');
 
-  return { router, middlewares, queryBuilder };
+  return { router, middlewares, queryBuilder, queryAsync };
 }
