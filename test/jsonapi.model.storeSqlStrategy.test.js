4d86bc64c21dab22652efe8ce1032384e03c78e8
Benoît Hubert <benoithubert@gmail.com>
2017-06-01 11:35:31 +0200

    get relationships for multiple records underway

--- test/jsonapi.model.storeSqlStrategy.test.js
+++ test/jsonapi.model.storeSqlStrategy.test.js
@@ -2,6 +2,7 @@ const utils = require('../lib/utils');
 const naming = require('../lib/naming');
 const path = require('path');
 const chai = require('chai');
+const _ = require('lodash');
 const should = chai.should();
 const expect = chai.expect();
 const Promise = require('bluebird');
@@ -17,6 +18,52 @@ const { store, descriptors } = jsonapi.model;
 const { queryBuilder, queryAsync } = jsonapi;
 
 const fakers = require('./fakers'); //(descriptors);
+
+function createPostWithRelatees() {
+  let postId;
+  let post;
+  return store.createRecord('user', fakers.user())
+  .then(user =>
+    store.createRecord('post', fakers.post(user.id))
+  )
+  // .get(utils.passLog('\n### User and Post'))
+  .then(_post => {
+    postId = _post.id;
+    post = _post;
+    const postComments = fakers.postComments(_post.id, 3);
+    const postMeta = fakers.postMeta(_post.id);
+    const tags = fakers.tags(4);
+    // console.log(postComments, postMeta, tags);
+    return Promise.all([
+      store.createRecord('postMeta', postMeta),
+      Promise.map(postComments, postComment =>
+        store.createRecord('postComment', postComment)
+      ),
+      Promise.map(tags, tag =>
+        store.createRecord('tag', tag)
+      )
+    ]);
+  })
+  .then(([postMeta, comments, tags]) => {
+    // console.log(postMeta);
+    return {
+      metaId: postMeta.id,
+      tagIds: tags.map(tag => (tag.id))
+    };
+  })
+  .then(({metaId, tagIds}) => Promise.all([
+    store.updateRecord('post', postId, { metaId }),
+    Promise.map(
+      tagIds.map(tagId => queryBuilder.insert('post_tag_tags', { postId, tagId })),
+      query => queryAsync(query)
+    )
+  ]))
+  .then(([_post, tags]) => {
+    post = _post;
+  })
+  .then(() => store.findAllRelatees('post', post))
+  .then(relatees => ({ post, relatees }))
+}
 
 describe('Test store SQL strategy', () => {
 
@@ -40,7 +87,7 @@ describe('Test store SQL strategy', () => {
     })
     .then(utils.passLog('posts'))
     // .get(({ user }) => store.findRelatees('user', user.id, 'posts'))
-    .get(({ user }) => store.findAllRelatees('user', user.id))
+    .get(({ user }) => store.findAllRelatees('user', user))
     .then(utils.passLog('relatees'))
     .then(() => done())
     .catch(err => {
@@ -49,44 +96,27 @@ describe('Test store SQL strategy', () => {
     });
   });
 
-  it('Creates user, posts, post meta, comments, tags', () => 
-    chain(store.createRecord('user', fakers.user()))
-    .set('user')
-    .then(user =>
-      store.createRecord('post', fakers.post(user.id))
-    )
-    .set('post')
-    // .get(utils.passLog('\n### User and Post'))
-    .then(post => {
-      const postComments = fakers.postComments(post.id, 3);
-      const postMeta = fakers.postMeta(post.id);
-      const tags = fakers.tags(4);
-      console.log(postComments, postMeta, tags);
-      return Promise.all([
-        store.createRecord('postMeta', postMeta),
-        Promise.map(postComments, postComment =>
-          store.createRecord('postComment', postComment)
-        ),
-        Promise.map(tags, tag =>
-          store.createRecord('tag', tag)
-        )
-      ]);
+  it.skip('Creates user, one post with post meta, comments, tags', () => 
+    createPostWithRelatees()
+    .then(utils.passLog('post and relatees'))
+    .catch(err => {
+      console.log('## err', err);
+      throw err;
     })
-    .set('relatees')
-    .then(utils.passLog('relatees'))
-    .then(([postMeta, comments, tags]) => {
-      return tags.map(tag => (tag.id));
-    })
-    .set('tagIds')
-    .get(({ post, tagIds }) => (
-      tagIds.map(tagId => queryBuilder.insert('post_tag_tags', { postId: post.id, tagId }))
-    ))
-    .then(queries => Promise.map(queries, query => queryAsync(query)))
-    .then(utils.passLog('pivot entries'))
-    .get(({ post }) => store.findAllRelatees('post', post))
-    .then(utils.passLog('all post relatees'))
+  );
 
-    // .then(() => done())
+  it('Creates three users, one post for each with post meta, comments, tags', () => 
+    Promise.all([
+      createPostWithRelatees(),
+      createPostWithRelatees(),
+      createPostWithRelatees()
+    ])
+    // .then(utils.passLog('post and relatees'))
+    .then(entries => (entries.map(entry => (entry.post))))
+    // .then(utils.passLog('posts only'))
+    .then(posts => store.findAllRelateesMulti('post', posts))
+    // .then(utils.passLog('posts relatees'))
+    .then(relatees => { console.log(_.map(relatees, 'meta')); })
     .catch(err => {
       console.log('## err', err);
       throw err;
